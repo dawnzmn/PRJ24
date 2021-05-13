6dd72fa1effe346b9132c1f6e1bfab83a4941428
dawnzmn <83068262+dawnzmn@users.noreply.github.com>
2021-05-13 18:03:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var rubber;
+var iron;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone = new Stone(600,500,90,90);
+    rubber = new Rubber(300,460,50,50);
+    iron = new Iron(800,500,40,60);
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    stone.display();
+    plane.display();
+    hammer.display();
+    rubber.display();
+    iron.display();
+}
